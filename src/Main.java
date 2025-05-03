a4a77a999e78927badda1f989290cb849a4abe22
arasruislam <arasru01@gmail.com>
2025-05-04 05:30:20 +0600

    feat: Initialize database tables in main method

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
 import javax.swing.*;
+
+import database.SchemaCreator;
 import gui.MainWindow;
 
 public class Main {
    public static void main(String[] args) {
+      // Create database tables
+      SchemaCreator.createTables();
+
+
       SwingUtilities.invokeLater(() -> {
          MainWindow window = new MainWindow();
          window.setVisible(true);
